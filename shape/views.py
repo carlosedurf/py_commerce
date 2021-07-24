9a4f81f0056c479bd786909398f9e1af1cb22844
Carlos Eduardo <carloseduardo.rocha.f@gmail.com>
2021-07-24 15:05:48 -0300

    create login and logout

--- shape/views.py
+++ shape/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
 from .forms import UserForm, ProfileForm
 from .models import Profile
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 import copy
 
 
@@ -111,7 +112,17 @@ class Create(BasePerfil):
         self.request.session['car'] = self.car
         self.request.session.save()
 
-        return self.render
+        messages.success(
+            self.request,
+            'Seu cadastro foi criado e/ou atualizado com sucesso!'
+        )
+
+        messages.success(
+            self.request,
+            'Você fez login e pode concluir sua compra!'
+        )
+
+        return redirect('profile:create')
 
 
 class Update(BasePerfil):
@@ -119,10 +130,42 @@ class Update(BasePerfil):
 
 
 class Login(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Login')
+    def post(self, *args, **kwargs):
+        username = self.request.POST.get('username')
+        password = self.request.POST.get('password')
+
+        if not username or not password:
+            messages.error(
+                self.request,
+                'Usuário ou senha inválidos.'
+            )
+            return redirect('profile:create')
+
+        user = authenticate(
+            self.request,
+            username=username,
+            password=password
+        )
+
+        if not user:
+            messages.error(
+                self.request,
+                'Usuário ou senha inválidos.'
+            )
+            return redirect('profile:create')
+
+        login(self.request, user=user)
+        messages.success(
+            self.request,
+            'Você fez login no sistema e pode concluir sua compra!'
+        )
+        return redirect('product:car')
 
 
 class Logout(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('Saindo')
+        car = copy.deepcopy(self.request.session.get('car'))
+        logout(self.request)
+        self.request.session['car'] = car
+        self.request.session.save()
+        return redirect('product:list')
